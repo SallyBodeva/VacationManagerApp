5b9cf36477d64c45cc23bb7579af597224512923
salihe <salihe.bodeva10@gmail.com>
2024-04-12 19:51:00 +0300

    Roles done

---
+++ VacationManagerApp/VacationManager.ViewModels/Roles/RolesEditViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VacationManagerApp.ViewModels.Roles
+{
+    public class RolesEditViewModel
+    {
+        public string RoleId { get; set; }
+        public string RoleName { get; set; }
+    }
+}
--- VacationManagerApp/VacationManagerApp.Web/Controllers/RolesController.cs
+++ VacationManagerApp/VacationManagerApp.Web/Controllers/RolesController.cs
@@ -1,58 +1,111 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
 using VacationManagerApp.Data;
-using VacationManagerApp.Services;
-using VacationManagerApp.Services.Contracts;
-using VacationManagerApp.ViewModels.Projects;
+using VacationManagerApp.Data.Models;
+using VacationManagerApp.ViewModels;
+using VacationManagerApp.Data;
 using VacationManagerApp.ViewModels.Roles;
 
-namespace VacationManagerApp.Web.Controllers
+namespace VacationManager.Controllers
 {
     public class RolesController : Controller
     {
-        private readonly ApplicationDbContext context;
-        private readonly IRoleService roleService;
+        private readonly UserManager<User> _userManager;
+        private readonly ApplicationDbContext _db;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public RolesController(ApplicationDbContext db, UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _db = db;
+            _roleManager = roleManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<IdentityRole> roles = _roleManager.Roles.ToList();
+            Dictionary<string, int> rolesMembersCount = new Dictionary<string, int>();
+            foreach (var role in roles)
+            {
+                var usersInRole = await _userManager.GetUsersInRoleAsync(role.Name);
+                rolesMembersCount.Add(role.Name, usersInRole.Count);
+            }
+            return View(rolesMembersCount);
+        }
+        public async Task<IActionResult> Members(string id)
+        {
+            if (await _roleManager.RoleExistsAsync(id))
+            {
+                RolesMembersViewModel data = new RolesMembersViewModel();
+                data.Members = new List<User>(await _userManager.GetUsersInRoleAsync(id));
+                data.RoleName = id;
+                return View(data);
+            }
+            return NotFound();
+        }
 
-        public RolesController(ApplicationDbContext context, IRoleService roleService)
+        public async Task<IActionResult> Edit(string id)
         {
-            this.context = context;
-            this.roleService = roleService;
+            if (!await _roleManager.RoleExistsAsync(id))
+            {
+                return NotFound();
+            }
+
+            RolesEditViewModel data = new RolesEditViewModel();
+            IdentityRole role = await _roleManager.FindByNameAsync(id);
+            data.RoleName = role.Name;
+            data.RoleId = role.Id;
+
+            return View(data);
         }
-        public async Task<IActionResult> Index(IndexRolesViewModel model)
+
+        [HttpPost]
+        public async Task<IActionResult> ChangeRoleName(RolesEditViewModel data)
         {
-            model = await roleService.GetRolesAsync(model);
-            return View(model);
+            IdentityRole role = await _roleManager.FindByIdAsync(data.RoleId);
+
+            if (role == null)
+            {
+                return NotFound();
+            }
+
+            role.Name = data.RoleName;
+            role.NormalizedName = data.RoleName.ToUpper();
+
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction("Members", new { id = data.RoleName });
         }
-        [HttpGet]
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(string roleName)
+        {
+            if (!await _roleManager.RoleExistsAsync(roleName))
+            {
+                return NotFound();
+            }
+
+            IdentityRole roleToDelete = await _roleManager.FindByNameAsync(roleName);
+
+            await _roleManager.DeleteAsync(roleToDelete);
+
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
         public IActionResult Create()
         {
             return View();
         }
+
+
         [HttpPost]
-        public async Task<IActionResult> Create(CreateRoleViewModel model)
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel data)
         {
-            if (ModelState.IsValid)
-            {
-                await roleService.CreateRole(model);
-                return RedirectToAction(nameof(Index));
-            }
-            return View(model);
+            await _roleManager.CreateAsync(new IdentityRole(data.NewRoleName));
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
         }
-
-        public async Task<IActionResult> Members(string id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var model = await roleService.Members(id);
-            if (model == null)
-            {
-                return NotFound();
-            }
-
-            return View(model);
-        }
-
     }
 }
